cf76660e6b3eefbe307254e1a48a41eb65fa2481
Allenhe123 <931317255@qq.com>
2019-06-20 22:28:42 +0800

    up

--- cmake_test1/cmake/modules/Findlibglog.cmake
+++ cmake_test1/cmake/modules/Findlibglog.cmake
@@ -1,11 +1,3 @@
-message(status, "using bundled Findlibdb.cmake...")
-find_path(
-    LIBGLOG_CXX_INCLUDE_DIR
-    glog.h
-    /usr/include/ /usr/local/include
-)
-
-find_library(
-    LIBGLOG_CXX_LIBRARIES NAMES glog
-    paths /usr/lib /usr/local/lib
-)
+message(status, "****using bundled Findlibglog.cmake...")
+find_path(LIBGLOG_CXX_INCLUDE_DIR logging.h /usr/include/ /usr/local/include/glog/)
+find_library(LIBGLOG_CXX_LIBRARIES glog /usr/lib/ /usr/local/lib/)
--- cmake_test1/main.cpp
+++ cmake_test1/main.cpp
@@ -1,9 +1,21 @@
 #include<iostream>
 #include "src/test1.h"
+#include "logging.h"
  
-int main()
+int main(int argc, char** argv)
 {
-     std::cout<<"Hello word!"<<std::endl;
-     print_msg();
-     return 0;
+    google::InitGoogleLogging((const char *)argv[0]);
+    google::SetLogDestination(google::GLOG_INFO,"./mylog.txt");
+    LOG(WARNING) <<"thisis the 1st warning!";
+
+    std::cout<<"Hello word!"<<std::endl;
+    print_msg();
+
+#ifdef _DEBUG
+     std::cout << "running in debug mode" << std::endl;
+#else
+     std::cout << "running in release mode" << std::endl;
+#endif // _DEBUG
+
+    return 0;
 }
